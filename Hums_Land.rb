16a1aa74d71d2a54b396ad1351047c31a819a082
carolyn arnold <carolyn.d.arnoldlp@gmail.com>
2017-10-06 03:04:40 +0000

    Project_1 Hums_Land fina l submission

--- Hums_Land.rb
+++ Hums_Land.rb
@@ -1,6 +1,6 @@
-This is my submission for I/O Avenue's Project 1 HumsLand Game.
+#This is my submission for I/O Avenue's Project 1 Hums Land Game.
 puts "Welcome to Hums Land: Virtual Realm!"
-puts "What's your desired experience abroad, you see?\n to experience Hums Land by?"
+puts "What's your desired experience abroad, you see?\n to travel within a foreign land by?"
 puts "Options: train, plane, sea, cherry tree"
 case option = gets.chomp
 when "train"
@@ -8,7 +8,7 @@ when "train"
 when "plane"
     puts "Zoom! Run onto Virgin Air."
 when "sea"
-    puts "Woohoo! All aboard Holland Cruise."
+    puts "Woohoo! All aboard Holland Cruise Line."
 when "cherry tree"
     puts "Up into the cherry tree, with hands and feet!\n Who should climb but little old me."
 end
@@ -20,10 +20,47 @@ when "landscapes"
     puts "I saw the next door gardern lie, adorn with flowers, before my eye,\n and many pleasant places more that I never seen before."
     puts "The dusty roads go up and down with people trampling in to town.\n If I could find a higher tree, farther and farther I should see."
 when "waterways"
-    puts "I saw the dimpling river pass and be the sky's blue looking glass.\n To where the grown up river slips into the sea among the ships.\n To where the roads n either hand, lead onward into fairy land."
+    puts "I saw the dimpling river pass and be the sky's blue looking glass.\n To where the grown up river slips into the sea among the ships.\n To where the roads on either hand, lead onward into fairy land."
 when "restaurants"
     puts "Where all the children dine at five, and all playthings come alive."
 end
 
-events_indoors = [museum, theater, concert]
-events_outdoors = [zoo, gardens, beach]
+puts "What did the children have for dinner?"
+puts "Say: Raspberries, Waffers, or Bubble Gum"
+case gets.chomp
+when "Raspberries"
+    puts "Yum, me love Raspberries!"
+when "Waffers"
+    puts "Yum, me love Waffers!"
+when "Bubble Gum"
+    puts "Yum, me love Bubble Gum!"
+else
+    puts "Yuk! Let's go and have fun with my playthings!"
+end
+
+puts "While the children dined at five, and all playthings came alive, the adults frolic in the streets."
+puts "Where did they go? zoo, gardens, museum, theater?"
+case gets.chomp
+when "zoo"
+    puts "All beings big and small, smelly and all."
+when "gardens"
+    puts "Fragrant and groomed with all a bloom." 
+when "museum"
+    puts "Glory, Glory! Looking past and forward."
+when "theater"
+    puts "'Hamilton' was Brilliant!"
+else
+    puts "Fa gida bout it, 'Hamilton' out shined other events!" 
+end
+
+puts "What was your favorite 'virtual' transportation?"
+    transport1 = {name: "train"}
+    transport2 = {name: "plane"}
+    transport3 = {name: "sea"}
+    transport4 = {name: "cherry tree"}
+    carriers = [transport1, transport2, transport3, transport4]
+for carrier in carriers
+        puts "Your transport #{carrier[:name]} was my fav!"
+end
+       puts "All was a delight."
+
